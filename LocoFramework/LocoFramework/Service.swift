228a1bfc6216b10475699febeff172b9a361e37a
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-06-13 14:51:56 +0530

    Finished our LocoFramework

---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Qaptive Technologies on 13/06/19.
+//  Copyright © 2019 Qaptive Technologies. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init () {}
+    
+    public static func doSomething() -> String {
+        return "This is done!!"
+    }
+}
